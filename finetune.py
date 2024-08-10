85785df35b76ac62af8434825b726133c1296591
DilankaSanjula <dilankasanjula@gmail.com>
2024-08-10 16:34:41 +0530

    start with model ckpt 8

--- finetune.py
+++ finetune.py
@@ -20,7 +20,7 @@ USE_MP = True
 # Paths
 dataset_visualize_image_path = "sample_batch_images.png"
 directory = '/content/drive/MyDrive/webvid-10m-dataset/grid_images_1'
-pretrained_weights_path = '/content/drive/MyDrive/models/ckpt_epoch_4.h5'
+pretrained_weights_path = '/content/drive/MyDrive/models/ckpt_epoch_8.h5'
 
 # Learning Parameters
 lr = 1e-5
