58c3480d6c395e521e3d49548c1418e36c0547a4
DilankaSanjula <dilankasanjula@gmail.com>
2024-08-11 12:27:54 +0530

    resume training

--- finetune.py
+++ finetune.py
@@ -20,7 +20,7 @@ USE_MP = True
 # Paths
 dataset_visualize_image_path = "sample_batch_images.png"
 directory = '/content/drive/MyDrive/webvid-10m-dataset/grid_images_1'
-pretrained_weights_path = '/content/drive/MyDrive/models/ckpt_epoch_4.h5'
+pretrained_weights_path = '/content/drive/MyDrive/models/ckpt_epoch_2_diffusion_model_lr.h5'
 pretrained_vae = '/content/drive/MyDrive/models/vae.h5'
 
 # Learning Parameters
