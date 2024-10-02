58720f1c62f4c827276582dd88542c91f0e26e61
DilankaSanjula <dilankasanjula@gmail.com>
2024-10-02 14:05:45 +0530

    removed save

--- finetune_decoder.py
+++ finetune_decoder.py
@@ -95,12 +95,12 @@ check_dataset_shapes(train_dataset)
 # Define the directory to save the dataset
 save_path = '/content/drive/MyDrive/models/decoder_dataset/'
 
-# Function to save dataset
-def save_dataset(dataset, path):
-    dataset.save(path)
+# # Function to save dataset
+# def save_dataset(dataset, path):
+#     dataset.save(path)
 
 # Example of saving the train_dataset
-save_dataset(train_dataset, save_path)
+#save_dataset(train_dataset, save_path)
 
 # # Function to load dataset
 # def load_dataset(path, element_spec):
