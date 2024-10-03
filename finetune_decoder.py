1fcfed3403d5ec06ed85701c205fe3232e264d39
DilankaSanjula <dilankasanjula@gmail.com>
2024-10-03 19:41:51 +0530

    Adjusted scaling

--- finetune_decoder.py
+++ finetune_decoder.py
@@ -10,28 +10,6 @@ stable_diffusion = StableDiffusion(
     img_width=img_width, img_height=img_height
 )
 
-#stable_diffusion.diffusion_model.load_weights("/content/drive/MyDrive/models/vae_diffusion_model/ckpt_epoch_100.h5_2x2_diffusion_model.h5")
-# # Load the decoder with pre-trained weights
-
-# prompts = ["Grid image of close up of handsome happy male professional typing on mobile phone in good mood"]
-# images_to_generate = 1
-# outputs = {}
-
-
-# for prompt in prompts:
-#     generated_latents = stable_diffusion.text_to_latent(
-#         prompt, batch_size=images_to_generate, unconditional_guidance_scale=40,num_steps=30
-#     )
-
-# generated_images = stable_diffusion.latent_to_image(generated_latents)
-
-# for i, image_array in enumerate(generated_images):
-#     img = Image.fromarray(image_array)
-#     file_path = "/content/drive/MyDrive/models/loaded_4x4_test.png"
-#     img.save(file_path)
-#     print(f"Saved: {file_path}")
-
-
 decoder = Decoder(512, 512)
 
 image_folder = '/content/drive/MyDrive/webvid-10-dataset-2/4x4_grid_images'
@@ -43,6 +21,7 @@ def preprocess_image(image_path):
     image = tf.image.decode_jpeg(image, channels=3)  # Assumes the images are in JPEG format
     image = tf.image.resize(image, (512, 512))       # Resize to the shape expected by the Decoder
     image = image / 255.0  # Normalize the images to [0, 1] range
+    image = (image / 127.5) - 1.0  # Rescaled the images to [-1, 1] range
     return image
 
 # Load image dataset
@@ -82,37 +61,37 @@ def load_image_and_latent(image_path, latent):
     latent = tf.squeeze(latent)  # Ensure latent tensor is in the correct shape
     return latent, image
 
-# # Ensure your decoder expects the correct input shape
-# def check_dataset_shapes(dataset):
-#     for latent, image in dataset.take(1):
-#         print("Latent shape:", latent.shape)
-#         print("Image shape:", image.shape)
+# Ensure your decoder expects the correct input shape
+def check_dataset_shapes(dataset):
+    for latent, image in dataset.take(1):
+        print("Latent shape:", latent.shape)
+        print("Image shape:", image.shape)
 
 
-# train_dataset = create_latent_image_dataset(image_folder)
-# check_dataset_shapes(train_dataset)
+train_dataset = create_latent_image_dataset(image_folder)
+check_dataset_shapes(train_dataset)
 
 #path = 'decoder_dataset/'
 path = '/content/drive/MyDrive/models/decoder_dataset'
 
-# def save_dataset(path):
-#     train_dataset.save(path)
+def save_dataset(path):
+    train_dataset.save(path)
 
-# save_dataset(path)
-
-
+save_dataset(path)
 
 
 
-# # # Load the dataset
-reloaded_dataset = tf.data.Dataset.load(path)
 
-# # Now `train_dataset` contains pairs of (latent, image) for training the decoder
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-loss_function = tf.keras.losses.MeanSquaredError()
 
-decoder.compile(optimizer=optimizer, loss=loss_function)
+# # # # Load the dataset
+# reloaded_dataset = tf.data.Dataset.load(path)
 
-# If shapes are correct, proceed to training
-history = decoder.fit(reloaded_dataset, epochs=40)
-decoder.save('/content/drive/MyDrive/models/decoder_4x4/decoder_4x4.h5')
+# # # Now `train_dataset` contains pairs of (latent, image) for training the decoder
+# optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+# loss_function = tf.keras.losses.MeanSquaredError()
+
+# decoder.compile(optimizer=optimizer, loss=loss_function)
+
+# # If shapes are correct, proceed to training
+# history = decoder.fit(reloaded_dataset, epochs=40)
+# decoder.save('/content/drive/MyDrive/models/decoder_4x4/decoder_4x4.h5')
