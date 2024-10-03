c1362db3e64dee94c4af6d30ac782d137f0a7d0c
DilankaSanjula <dilankasanjula@gmail.com>
2024-10-03 22:03:19 +0530

    adjusted params

--- finetune_decoder.py
+++ finetune_decoder.py
@@ -87,12 +87,18 @@ path = '/content/drive/MyDrive/models/decoder_dataset'
 # # # Load the dataset
 reloaded_dataset = tf.data.Dataset.load(path)
 
+train_size = int(0.8 * len(reloaded_dataset))  # 80% for training
+val_size = len(reloaded_dataset) - train_size
+
+train_dataset = reloaded_dataset.take(train_size)
+val_dataset = reloaded_dataset.skip(train_size)
+
 # # Now `train_dataset` contains pairs of (latent, image) for training the decoder
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
 loss_function = tf.keras.losses.MeanSquaredError()
 
 decoder.compile(optimizer=optimizer, loss=loss_function)
 
 # If shapes are correct, proceed to training
-history = decoder.fit(reloaded_dataset, epochs=3)
+history = decoder.fit(train_dataset, validation_data=val_dataset, epochs=10)
 decoder.save('/content/drive/MyDrive/models/decoder_4x4/decoder_4x4.h5')
