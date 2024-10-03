205c89f03b747b113abe4234a316fa235bc03d67
DilankaSanjula <dilankasanjula@gmail.com>
2024-10-03 22:21:38 +0530

    debug

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ grid_model = StableDiffusion(
 # We just reload the weights of the fine-tuned diffusion model.
 grid_model.diffusion_model.load_weights("/content/drive/MyDrive/models/models/ckpt_epoch_100.h5_2x2_diffusion_model.h5")
 
-grid_model.decoder.load_weights("/content/drive/MyDrive/models/decoder_4x4/decoder_4x4.h5")
+#grid_model.decoder.load_weights("/content/drive/MyDrive/models/decoder_4x4/decoder_4x4.h5")
 
 prompts = ["Grid image of close up of handsome happy male professional typing on mobile phone in good mood"]
 images_to_generate = 1
@@ -24,6 +24,9 @@ for prompt in prompts:
     )
 
 generated_images = grid_model.latent_to_image(generated_latents)
+
+print("Decoded image min value:", generated_images.min())
+print("Decoded image max value:", generated_images.max())
 
 for i, image_array in enumerate(generated_images):
     img = Image.fromarray(image_array)
