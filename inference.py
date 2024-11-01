51fdfd3e4ddabbd22a4407ec7ecf0a587fac0f8f
DilankaSanjula <dilankasanjula@gmail.com>
2024-11-01 16:32:53 +0530

    Modified inference script

--- inference.py
+++ inference.py
@@ -12,7 +12,7 @@ grid_model = StableDiffusion(
 grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_4x4_scaled_linear_simpsons/ckpt_epoch_100.h5_2x2_diffusion_model.h5")
 
 #grid_model.decoder.load_weights("/content/drive/MyDrive/models/decoder_4x4/decoder_4x4.h5")
-#grid_model.decoder.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/decoder_model_scaled_linear/decoder3.h5")
+grid_model.decoder.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/decoder_model_scaled_linear/decoder_simpsons.h5")
 
 #prompts = ["Grid image of close up of handsome happy male professional typing on mobile phone in good mood"]
 prompts = ["homer simpson escapes fire"]#
@@ -31,7 +31,7 @@ print("Decoded image max value:", generated_images.max())
 
 for i, image_array in enumerate(generated_images):
     img = Image.fromarray(image_array)
-    file_path = f"/content/drive/MyDrive/stable_diffusion_4x4/scaled_linear_7.5_no_decoder_simpsons_1{i}.png"
+    file_path = f"/content/drive/MyDrive/stable_diffusion_4x4/scaled_linear_7.5_no_decoder_simpsons_77{i}.png"
     img.save(file_path)
     print(f"Saved: {file_path}")
     
