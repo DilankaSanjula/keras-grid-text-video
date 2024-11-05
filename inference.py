b13af6eec1f6cef10648b5b421c977dfb8f3536a
DilankaSanjula <dilankasanjula@gmail.com>
2024-11-05 12:57:08 +0530

    test 2048

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ grid_model = StableDiffusion(
 # We just reload the weights of the fine-tuned diffusion model.
 #grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_1/ckpt_epoch_70.h5_2x2_diffusion_model.h5")
 #grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_2/ckpt_epoch_100.h5_2x2_diffusion_model.h5")
-grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_4_2048/epoch_80.h5_2x2_diffusion_model.h5")
+grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_4_2048/epoch_100.h5_2x2_diffusion_model.h5")
 
 #grid_model.decoder.load_weights("/content/drive/MyDrive/models/decoder_4x4/decoder_4x4.h5")
 #grid_model.decoder.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/decoder_model_scaled_linear/decoder_simpsons2.h5")
