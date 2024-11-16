708e7c2747bb31b904ff514ebeb9b3b4e9fc9424
DilankaSanjula <dilankasanjula@gmail.com>
2024-11-16 10:55:07 +0530

    increased batch size to 8

--- finetune_encoder_decoder.py
+++ finetune_encoder_decoder.py
@@ -44,8 +44,8 @@ def prepare_grid_dataset(image_paths, batch_size=4):  # Reduced batch size
     return dataset
 
 # Create datasets
-train_dataset = prepare_grid_dataset(train_paths, batch_size=4)
-val_dataset = prepare_grid_dataset(val_paths, batch_size=4)
+train_dataset = prepare_grid_dataset(train_paths, batch_size=8)
+val_dataset = prepare_grid_dataset(val_paths, batch_size=8)
 
 # Initialize the Encoder and Decoder
 encoder = ImageEncoder(download_weights=False)
