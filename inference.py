7271b1aa7f2d993ff5374a15ff2f925a6cc7b18b
DilankaSanjula <dilankasanjula@gmail.com>
2024-12-22 09:03:31 +0530

    fix(model-loading): update weights path to stage 4 epoch 100

--- inference.py
+++ inference.py
@@ -11,7 +11,7 @@ grid_model = StableDiffusion(
 # We just reload the weights of the fine-tuned diffusion model.
 #grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_1/ckpt_epoch_70.h5_2x2_diffusion_model.h5")
 #grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_2/stage2.h5")
-grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_6/best.h5")
+grid_model.diffusion_model.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/diffusion_model_stage_4/epoch_100.h5_2x2_diffusion_model.h5")
 
 grid_model.decoder.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/decoder_encoder_training/best_vae_decoder.h5")
 #grid_model.decoder.load_weights("/content/drive/MyDrive/stable_diffusion_4x4/decoder_model_scaled_linear/decoder_simpsons2.h5")
